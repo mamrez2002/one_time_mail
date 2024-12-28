d26313b932649a4b2053f6a702266c2d5b497162
mamrez <mamrez.2002.s@gmail.com>
2024-12-28 16:56:34 +0330

    fix static error

--- otm/settings.py
+++ otm/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
